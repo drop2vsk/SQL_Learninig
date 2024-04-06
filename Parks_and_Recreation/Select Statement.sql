4086bf51b47579cbd572d70c058703e554648afb
Sureshkumar V <drop2vsk@gmail.com>
2024-04-06 21:05:16 +0530

    Select Statement tutorial

---
+++ Parks_and_Recreation/Select Statement.sql
@@ -0,0 +1,12 @@
+-- data manipulation language (DML) command - SELECT
+Select * from parks_and_recreation.employee_demographics; -- we are select the all record from parks_and_recreation<Schema>.employee_demographics<Table>
+
+SELECT first_name, 
+last_name, 
+birth_date, /* select required column from the table*/
+age, 
+(60 - age) as 'time for Retirement' /* Do reqired math 'PEMDAS is the order of exciution in SQL'*/
+FROM parks_and_recreation.employee_demographics;
+
+Select distinct gender /* select unique rows from the table using DISTINCT*/
+from parks_and_recreation.employee_demographics;
