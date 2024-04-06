6376c9e29fb5b6fc13ade6341895f4cec0fa00ef
Sureshkumar V <drop2vsk@gmail.com>
2024-04-06 21:24:43 +0530

    Where clause tutorial

---
+++ Parks_and_Recreation/Where clause.sql
@@ -0,0 +1,14 @@
+# Where clause 
+/*  ++ Operators, we can used with WHERE clause are given below ++
+	% Relational Operator %
+	=  - Equal to
+	<  - Less than
+    <= - Less than equal to
+    >  - Greater than
+    >= - Greater than equal to
+    != - Not equal to 
+*/
+
+Select *
+From parks_and_recreation.employee_salary
+Where salary <= 50000; -- here we are filter by salary by using lesser than(<=) and also we can use above
