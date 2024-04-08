a2840eca4268fee121a4a77fb177b5da95488b03
Sureshkumar V <drop2vsk@gmail.com>
2024-04-08 21:17:21 +0530

    Where clause completed

--- Parks_and_Recreation/Where clause.sql
+++ Parks_and_Recreation/Where clause.sql
@@ -6,13 +6,31 @@
     <= - Less than equal to
     >  - Greater than
     >= - Greater than equal to
-    != - Not equal to 
+    != - Not equal to 	
     
     % Logical Operator %
     AND
     OR
+    NOT
+    
+    $ LIKE Statement $
+    % => anything 
+    _ => exact
+    
 */
 
 Select *
 From parks_and_recreation.employee_salary
 Where salary <= 50000; -- here we are filter by salary by using lesser than(<=) and also we can use above
+
+select * 
+from parks_and_recreation.employee_demographics
+where gender = 'Male' and age > 20 and not age >= 40; -- here we are filter the demographic table by male and age between 20 to 39
+
+select *
+from parks_and_recreation.employee_demographics
+where first_name like 'A%'; -- here Any n number of character can come after "A"
+
+select *
+from parks_and_recreation.employee_demographics
+where first_name like 'A__'; -- here only 2 character can come after "A"
