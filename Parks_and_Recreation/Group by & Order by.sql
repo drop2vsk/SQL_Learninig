db32aa158c963fab277eb1031e84cf84795996dc
Sureshkumar V <drop2vsk@gmail.com>
2024-04-10 19:16:13 +0530

    Learning order by
    Signed-off-by: Sureshkumar V <drop2vsk@gmail.com>

--- Parks_and_Recreation/Group by & Order by.sql
+++ Parks_and_Recreation/Group by & Order by.sql
@@ -7,5 +7,16 @@ group by gender;
 -- Order by
 select * 
 from parks_and_recreation.employee_demographics
-order by first_name /*<= By default Order by is using "ASC" if we want descending order we need to give expilicity "DESC" keyword*/
-;
+order by first_name; /*<= By default Order by is using "ASC" if we want descending order we need to give expilicity "DESC" keyword*/
+
+select * 
+from parks_and_recreation.employee_demographics
+order by gender,age; /*<= Here First order by apply on gender (unique values) than the age*/
+
+select * 
+from parks_and_recreation.employee_demographics
+order by age,gender; /*<= Here order by age than the gender give wrong output */
+
+select employee_id,first_name,last_name,age,gender,birth_date
+from parks_and_recreation.employee_demographics
+order by 5;  /*<= Here order by using column position instead of column name */
