e2ae5123f13c3c8028e30b5cdd7630c5e714c1fc
Sureshkumar V <drop2vsk@gmail.com>
2024-05-06 13:27:51 +0530

    Outer join update

--- Parks_and_Recreation/Joins.sql
+++ Parks_and_Recreation/Joins.sql
@@ -12,13 +12,41 @@ from parks_and_recreation.employee_demographics as dem
 Join parks_and_recreation.employee_salary as sal
 	on dem.employee_id = sal.employee_id;
     
-select employee_id, first_name, occupation /* this query is failed because the employee_id is ambiguous(having more than one possible) 
-	because the employee_id is present in both table*/
+select employee_id, first_name, occupation /* this query is failed because the employee_id & first_name are ambiguous(having more than one possible) 
+	because the employee_id & first_name are present in both table*/
 from employee_demographics
 join employee_salary
 	on employee_demographics.employee_id = employee_salary.employee_id ;
--- correct way below
-select dem.employee_id, first_name, occupation 
+    
+-- correct way in below
+select dem.employee_id, dem.first_name, occupation 
 from employee_demographics as dem
 join employee_salary as sal
-	on employee_demographics.employee_id = employee_salary.employee_id
+	on dem.employee_id = sal.employee_id;
+    
+-- outer join 
+/* Outer joins
+Outer joins are joins that return matched values and unmatched values from either or both tables. 
+There are a few types of outer joins:
+
+LEFT JOIN returns only unmatched rows from the left table, as well as matched rows in both tables.LEFT JOIN is also refered to as OUTER LEFT JOIN.
+RIGHT JOIN returns only unmatched rows from the right table , as well as matched rows in both tables.RIGHT JOIN is also refered to as OUTER RIGHT JOIN.
+FULL OUTER JOIN returns unmatched rows from both tables,as well as matched rows in both tables.FULL OUTER JOIN is also refered to as OUTER JOIN.*/
+
+-- Left join
+select * 
+from parks_and_recreation.employee_demographics as dem
+left Join parks_and_recreation.employee_salary as sal
+	on dem.employee_id = sal.employee_id; 
+    
+-- Right join
+select * 
+from parks_and_recreation.employee_demographics as dem
+right Join parks_and_recreation.employee_salary as sal
+	on dem.employee_id = sal.employee_id;
+    
+-- full outer join
+select * 
+from parks_and_recreation.employee_demographics as dem
+Full outer join parks_and_recreation.employee_salary as sal
+	on dem.employee_id = sal.employee_id;
